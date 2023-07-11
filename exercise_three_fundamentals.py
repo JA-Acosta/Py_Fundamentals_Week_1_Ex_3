db0b9c1237e0a67a4ecb160b718a43a9eaee9923
JAAR <joe.aaron.acosta@outlook.com>
2023-07-11 16:27:26 -0400

    Version 0

---
+++ exercise_three_fundamentals.py
@@ -0,0 +1,15 @@
+'''
+>>> JAAR
+>>> 07/11/2023
+>>> Practicing Fundamentals Program 3
+>>> Version 0
+'''
+'''
+>>> Generates a program that checks if a string is a palindrome and will reverse the string for the user to verify. The string length will then be used to generate a random number (inclusive) which will serve as n for the fibonacci sequence. Finally, a total of 5 numbers will be selected from the same range to calculate an average.
+'''
+
+def main() :
+    print('Hello World!')
+
+if __name__ == '__main__' :
+    main()
